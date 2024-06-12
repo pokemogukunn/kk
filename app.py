3b3482ad78dc4ab6940383e52fdb7428ccbfd11f
pokemogukunn <156386753+pokemogukunn@users.noreply.github.com>
2024-06-13 07:51:17 +0900

    Update app.py

--- app.py
+++ app.py
@@ -12,6 +12,8 @@ def index():
 def search():
     query = request.args.get('q')
     api_key = os.getenv('API_KEY')
+    if not api_key:
+        return jsonify({"error": "APIキーが設定されていません"}), 500
     url = f"https://www.googleapis.com/youtube/v3/search?part=snippet&q={query}&key={api_key}"
     response = requests.get(url)
     data = response.json()
