c2a1e00fd2b44250d2c6f3de12a43b63b57f1672
pokemogukunn <156386753+pokemogukunn@users.noreply.github.com>
2024-06-12 21:52:13 +0900

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("public/index.html")
 
 @app.route("/search")
 def search():
